b72ce48a987dfff55384622a2cd92856d5ad6631
Brittni <brittni.toporek@gmail.com>
2023-02-20 17:35:32 -0700

    final project committed

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -5,6 +5,7 @@ function NotFoundPage() {
     <div className="not-found">
       <a href="/">
         <img
+          className="error-image"
           src="https://img.freepik.com/free-vector/internet-network-warning-404-error-page-file-found-web-page-internet-error-page-issue-found-network-404-error-present-by-man-sleep-display_1150-55450.jpg?w=1380&t=st=1676854265~exp=1676854865~hmac=68a5873864fb8a96576826ab655dcda63f6376d84f3ab43c4043052899809c11"
           alt=""
         />
