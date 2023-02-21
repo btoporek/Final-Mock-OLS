e26a108ded1439427ebe7281381e460a2598591f
Brittni <brittni.toporek@gmail.com>
2023-02-20 19:42:56 -0700

    fixed edit courses to prevent blank submits

--- src/components/EditCourseModal.js
+++ src/components/EditCourseModal.js
@@ -1,14 +1,20 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import React, { useState } from "react";
-import { FormControl, FormGroup, FormLabel } from "react-bootstrap";
+import { Form, FormControl, FormGroup, FormLabel } from "react-bootstrap";
 
 function EditCourseModal({ id, name, description, fetchCourses }) {
   const [showCourse, setShowCourse] = useState(false);
   const [courseName, setCourseName] = useState("");
   const [courseDescription, setCourseDescription] = useState("");
+  const [nameChange, setNameChange] = useState(false);
+  const [descriptionChange, setDescriptionChange] = useState(false);
+  const [disabledButton, setDisabledButton] = useState(true);
 
-  const handleCloseCourse = () => setShowCourse(false);
+  const handleCloseCourse = () => {
+    setShowCourse(false);
+    setDisabledButton(true);
+  };
   const handleShowCourse = () => setShowCourse(true);
 
   // Function to edit courses in api and page
@@ -38,6 +44,27 @@ function EditCourseModal({ id, name, description, fetchCourses }) {
     handleCloseCourse();
   };
 
+  //function to save state of changed value for course name - checks if statement to enable save button
+  const changeDescription = (value) => {
+    setCourseDescription(value);
+    setDescriptionChange(true);
+    enableSave();
+  };
+
+  //function to save state of changed value for course name - checks if statement to enable save button
+  const changeName = (value) => {
+    setCourseName(value);
+    setNameChange(true);
+    enableSave();
+  };
+
+  //function to enable save button if both values have been changed
+  const enableSave = () => {
+    if (nameChange === true && descriptionChange === true) {
+      setDisabledButton(false);
+    }
+  };
+
   return (
     <>
       <button className="card-btn-edit" onClick={handleShowCourse}>
@@ -51,27 +78,28 @@ function EditCourseModal({ id, name, description, fetchCourses }) {
       >
         <Modal.Header id="edit-header">Edit Course: {name}</Modal.Header>
         <Modal.Body>
-          <FormGroup>
+          <form>
             <FormLabel>Course Name:</FormLabel>
             <FormControl
               type="text"
               placeholder={name}
               value={courseName}
               onChange={(e) => {
-                setCourseName(e.target.value);
+                changeName(e.target.value);
               }}
             />
             <FormLabel>Course Description:</FormLabel>
             <FormControl
               as="textarea"
+              aria-required
               rows={3}
               placeholder={description}
               value={courseDescription}
               onChange={(e) => {
-                setCourseDescription(e.target.value);
+                changeDescription(e.target.value);
               }}
             />
-          </FormGroup>
+          </form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseCourse}>
@@ -79,6 +107,8 @@ function EditCourseModal({ id, name, description, fetchCourses }) {
           </Button>
           <Button
             variant="primary"
+            type="submit"
+            disabled={disabledButton}
             onClick={(e) => {
               handleEditCourse(e, id);
             }}
